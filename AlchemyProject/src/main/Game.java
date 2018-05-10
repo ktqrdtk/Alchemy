36a1a3cdc795352b85c8d7244720baff2851631b
KTQRDTK <KTQRDTK@CCSD-45JYBH2.ccsdut.net>
2018-05-10 14:47:54 -0600

    commit

--- AlchemyProject/src/main/Game.java
+++ AlchemyProject/src/main/Game.java
@@ -1,6 +1,54 @@
 package main;
 
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.JFrame;
+
 public class Game
 {
-
+	public static void main(String... args)
+	{
+		new Game();
+	}
+	
+	private JFrame frame;
+	
+	public Game()
+	{
+		setUpScreen();
+		Pictures.setUp();
+	}
+	
+	public void setUpScreen()
+	{
+		frame = new JFrame("Alchemy")
+				{
+					public void paint(Graphics g)
+					{
+						g.drawImage(Pictures.scaledIcons[0].getImage(), 0, 0, null);
+					}
+				};
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ComponentAdapter ca = new ComponentAdapter()
+				{
+					public void componentResized(ComponentEvent ev)
+					{
+						Pictures.setScaledIcons(smartSize(ev.getComponent()), smartSize(ev.getComponent()));
+					}
+				};
+				
+		frame.getRootPane().addComponentListener(ca);
+		ca.componentResized(new ComponentEvent(frame.getRootPane() ,101));
+		frame.pack();
+		frame.repaint();
+	}
+	
+	public int smartSize(Component input)
+	{
+		return 5;
+	}
 }
---
+++ AlchemyProject/src/main/Pictures.java
@@ -0,0 +1,82 @@
+package main;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class Pictures 
+{
+	private static String earthLocation, fireLocation, waterLocation, airLocation;
+	private static final String stringStarter = "Images/";
+	private static Image[] images;
+	private static ImageIcon[] originalIcons;
+	public static ImageIcon[] scaledIcons;
+	
+	public static void setUp()
+	{
+		setLocations();
+		setImages();
+		setOriginalIcons();
+	}
+	
+	private static void setLocations()
+	{
+		earthLocation = stringStarter + "earth.png";
+		fireLocation = stringStarter + "fire.png";
+		waterLocation = stringStarter + "water.png";
+		airLocation = stringStarter + "air.png";
+	}
+	
+	private static void setImages()
+	{
+		try
+		{
+			Image earthImg = ImageIO.read(new File(earthLocation));
+			Image fireImg = ImageIO.read(new File(fireLocation));
+			Image waterImg = ImageIO.read(new File(waterLocation));
+			Image airImg = ImageIO.read(new File(airLocation));
+			
+			Image[] tempArray = {earthImg, fireImg, waterImg, airImg};
+			images = tempArray;
+		}
+		catch(IOException ex)
+		{
+			System.out.println("Missing Images");
+		}
+	}
+	
+	private static void setOriginalIcons()
+	{
+		ImageIcon earthOIcon = new ImageIcon(images[0]);
+		ImageIcon fireOIcon = new ImageIcon(images[1]);
+		ImageIcon waterOIcon = new ImageIcon(images[2]);
+		ImageIcon airOIcon = new ImageIcon(images[3]);
+		ImageIcon[] tempArray = {earthOIcon, fireOIcon, waterOIcon, airOIcon};
+		originalIcons = tempArray;
+		scaledIcons = tempArray;
+	}
+	
+	public static void setScaledIcons(int _1, int _2)
+	{
+		int new_1 = smartCast(_1);
+		int new_2 = smartCast(_2);
+		for(int i = 0; i < originalIcons.length; i++)
+		{
+			scaledIcons[i].setImage(scaleImageIcon(originalIcons[i], new_1, new_2));
+		}
+	}
+	
+	private static int smartCast(double input) 
+	{
+		return (int)(input + .5);
+	}
+
+	private static Image scaleImageIcon(ImageIcon input, int _1, int _2)
+	{
+		Image returnValue = input.getImage().getScaledInstance(_1, _2, java.awt.Image.SCALE_SMOOTH);
+		return returnValue;
+	}
+}
