190387e07dfcdab7e036933a4fc2c82cdebcd056
KTQRDTK <KTQRDTK@CCSD-45JYBH2.ccsdut.net>
2018-05-15 14:50:10 -0600

    a

--- AlchemyProject/src/main/Game.java
+++ AlchemyProject/src/main/Game.java
@@ -1,19 +1,15 @@
 package main;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
 
 public class Game
 {
@@ -23,7 +19,7 @@ public class Game
 	}
 	
 	private JFrame frame;
-	private JPanel mainPane;
+	private JPanel mainPanel;
 	
 	public Game()
 	{
@@ -36,7 +32,7 @@ public class Game
 		frame = new JFrame("Alchemy");
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainPane = new JPanel()
+		mainPanel = new JPanel()
 				{
 					public void paintComponent(Graphics g)
 					{
@@ -44,7 +40,7 @@ public class Game
 						g.drawImage(Pictures.scaledIcons[4].getImage(), 0, 0, null);
 					}
 				};
-		mainPane.setBackground(new Color(102, 204, 0));
+		mainPanel.setBackground(Color.YELLOW);
 		
 		ComponentAdapter coAd = new ComponentAdapter()
 				{
@@ -53,34 +49,17 @@ public class Game
 						Pictures.setScaledIcons(smartSize(ev.getComponent()), smartSize(ev.getComponent()));
 					}
 				};
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frame.getContentPane().add(mainPanel);
+		mainPanel.setLayout(new MigLayout("", "[grow][grow]", "[grow][grow][]"));
 		
-		frame.getContentPane().add(mainPane);
-		frame.getRootPane().addComponentListener(coAd);
-		coAd.componentResized(new ComponentEvent(frame.getRootPane(), 101));
-		GridBagLayout gbl_mainPane = new GridBagLayout();
-		gbl_mainPane.columnWidths = new int[] {10, 10, 10, 0};
-		gbl_mainPane.rowHeights = new int[]{10, 0};
-		gbl_mainPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_mainPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		mainPane.setLayout(gbl_mainPane);
+		JPanel inventoryPanel = new JPanel();
+		inventoryPanel.setBackground(Color.BLUE);
+		mainPanel.add(inventoryPanel, "cell 1 0,grow");
 		
-		JPanel mixingPane = new JPanel();
-		mixingPane.setBackground(Color.BLACK);
-		GridBagConstraints gbc_mixingPane = new GridBagConstraints();
-		gbc_mixingPane.anchor = GridBagConstraints.NORTHWEST;
-		gbc_mixingPane.insets = new Insets(0, 0, 0, 5);
-		gbc_mixingPane.gridx = 1;
-		gbc_mixingPane.gridy = 0;
-		mainPane.add(mixingPane, gbc_mixingPane);
-		JPanel inventoryPane = new JPanel();
-		inventoryPane.setPreferredSize(new Dimension(inventoryPane.getPreferredSize().width, inventoryPane.getPreferredSize().height));
-		inventoryPane.setBackground(Color.BLUE);
-		GridBagConstraints gbc_inventoryPane = new GridBagConstraints();
-		gbc_inventoryPane.anchor = GridBagConstraints.NORTHWEST;
-		gbc_inventoryPane.gridx = 2;
-		gbc_inventoryPane.gridy = 0;
-		mainPane.add(inventoryPane, gbc_inventoryPane);
-		
+		JPanel mixingPanel = new JPanel();
+		mixingPanel.setBackground(Color.GREEN);
+		mainPanel.add(mixingPanel, "cell 0 0,grow");
 		frame.pack();
 		frame.repaint();
 		frame.setVisible(true);
